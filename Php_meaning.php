807a79abdbce54e47f2fcf23fd0809ecea794782
mawubaskyblair-cmyk <mawubaskyblair@gmail.com>
2026-08-29 11:24:37 +0300

    Add files via upload

---
+++ Php_meaning.php
@@ -0,0 +1,17 @@
+#############################
+## This is the most powerfull server_side scripting language for making dynamic and interactive web pages.
+#############################
+
+# FOR EXAMPLE.
+# Write you fast Php fast script you know.
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo phpversion();
+?> 
+
+</body>
+</html>
