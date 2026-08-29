a5ae029b2bb6499b378c4986fd461ae0fca14c35
mawubaskyblair-cmyk <mawubaskyblair@gmail.com>
2026-08-29 11:24:14 +0300

    Add files via upload

---
+++ Comments.php
@@ -0,0 +1,17 @@
+## have only 2 diffferent comments
+
+// This is a single-line comment
+# This is also a single-line comment
+
+/* This is a
+multi-line
+comment */
+
+# A C omment before the command line #
+
+// Output welcome message
+echo 'Welcome Home!';
+
+## A Comment affter the command line #
+
+echo 'Welcome Home!'; // Output welcome message
