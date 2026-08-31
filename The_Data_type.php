07f3435c2711614b5722625fd3e4c77663fd376c
mawubaskyblair-cmyk <mawubaskyblair@gmail.com>
2026-08-31 10:30:28 +0300

    Add files via upload

---
+++ The_Data_type.php
@@ -0,0 +1,12 @@
+/*PHP supports the following data types:
+
+string (text values)
+int (whole numbers)
+float (decimal numbers)
+bool (true or false)
+array (multiple values)
+object (stores data as objects)
+null (empty variable)
+resource (references external resources)
+mixed (any value)
+*/
